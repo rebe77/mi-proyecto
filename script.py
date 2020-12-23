f8d7bbc1056f8460d2ba7f4cbbf359a88509000f
Rebeca Freire <rfreireu@est.ups.edu.ec>
2020-12-22 21:23:36 -0500

    Commit inicial

---
+++ script.py
@@ -0,0 +1,10 @@
+# Script de prueba para Git
+# Autor: Rebeca Freire <rfreireu@est.ups.edu.ec>
+# Fecha: 22-12-2020
+# Versión: 1.0.0
+
+def main():
+    print("hello world!")
+
+if __name__ == "__main__":
+    main()
